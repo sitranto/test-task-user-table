67cb2b4266e06296b284af67e9281468a50113de
sitranto <anatoliyl2006@gmail.com>
2025-08-10 10:26:43 +0300

    Basic pagination

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import Table from "./components/Table/Table.tsx";
 import {useEffect, useState} from "react";
 import type {User} from "./types/user.ts";
 import {getUsers} from "./api/usersApi.ts";
+import Pagination from "./components/Pagination/Pagination.tsx";
 
 const LIMIT = 10;
 
 function App() {
     const [userData, setUserData] = useState<User[]>([]);
-    const [currentPage, setCurrentPage] = useState(1);
+    const [currentPage, setCurrentPage] = useState(Number(localStorage.getItem("current_page")));
+
+    useEffect(() => {
+        if (!currentPage) {
+            setCurrentPage(1);
+        }
+    }, []);
 
     useEffect(() => {
         getUsers(
@@ -17,11 +24,12 @@ function App() {
         ).then((data) => {
             setUserData(data.users);
         });
-    }, [])
+    }, [currentPage])
 
   return (
     <>
-      <Table data={userData} />
+        <Table data={userData} />
+        <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} />
     </>
   )
 }
---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,36 @@
+import {useState} from "react";
+
+interface PaginationProps {
+    currentPage: number;
+    setCurrentPage: (currentPage: number) => void;
+}
+
+export default function Pagination({ currentPage, setCurrentPage }: PaginationProps) {
+    const [inputValue, setInputValue] = useState(currentPage.toString());
+
+    function handlePageChange(page: number) {
+        if (page <=0) {
+            page = 1;
+        }
+        setCurrentPage(page);
+        setInputValue(page.toString());
+        localStorage.setItem('current_page', page.toString());
+    }
+    return (
+        <>
+            <button onClick={() => handlePageChange(currentPage - 1)}>
+                Назад
+            </button>
+            <input
+                type="number"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onBlur={() => handlePageChange(Number(inputValue))}
+                onKeyUp={(e) => { if (e.key === "Enter") { e.currentTarget.blur() }}} // по сути вызов другого обработчика TODO проверить можно ли заменить
+            />
+            <button onClick={() => handlePageChange(currentPage + 1)}>
+                Вперед
+            </button>
+        </>
+    );
+}
